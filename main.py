003cb927fa42ab5b26650fbe8823f273219cefe7
Ivan Skliaruk <cunanori@gmail.com>
2024-02-07 13:14:25 +0100

    last commit

---
+++ main.py
@@ -0,0 +1,73 @@
+import requests
+import json
+from src.classes import Api_call, Vacancy, JSON_saver
+from pprint import pprint
+
+key = 'https://api.hh.ru/vacancies'
+
+def user_interaction():
+
+    to_json = JSON_saver()
+    api_call_result = []
+
+
+    def user_search(request):
+        call = Api_call()
+        vacancies = []
+
+        for vac in call(key, request)["items"]:
+            vacancies.append(Vacancy(vac).__dict__)
+
+        api_call_result.extend(vacancies)
+
+    def get_top_n():
+        return user_request
+
+    def key_word_search(request):
+        vacansies_with_key = []
+        for vac in api_call_result:
+            for value in vac.values():
+                if isinstance(value, str) and request in value:
+                    vacansies_with_key.append(vac)
+
+        return vacansies_with_key
+
+    cnt = 0
+    while not api_call_result:
+        if cnt == 0:
+            user_request = input("Введите ключевое слово для поиска: ").lower()
+            user_search(user_request)
+            cnt += 1
+        else:
+            user_request = input("Мы ничего не нашли по вашему запросу. Задайте новый, например - 'java разработчик': ").lower()
+            user_search(user_request)
+
+    to_json(api_call_result, 'data/vacansies.json')
+    api_call_result.sort(key=lambda vac: vac["from_sal"] if vac["from_sal"] else vac["to_sal"])
+
+    try:
+        user_request = int(input("Далее мы покажем вам вакансии с лучшими зарплатами.\n"
+                                 "Введите цифрой количество вакансий, которое вас интересует (по умолчанию покажем 5): "))
+        pprint(api_call_result[:-get_top_n()-1:-1])
+    except Exception:
+        pprint(api_call_result[:-6:-1])
+
+
+    user_request = 'aaaaaaaaaaaaaaaaaaaaaaa'
+    cnt1 = 0
+    while not key_word_search(user_request):
+        if cnt1 == 0:
+            user_request = input(
+                "Вы также можете получить список вакансий, которые содержат дополнительное ключевое слово.\n"
+                "Введите слово для поиска: ").lower()
+            pprint(key_word_search(user_request))
+            cnt1 += 1
+        else:
+            user_request = input(
+                "По заданному запросу ничего не нашли.\n"
+                "Введите новое слово для поиска: ").lower()
+            pprint(key_word_search(user_request))
+
+    return 'Спасибо за использование :)'
+
+print(user_interaction())
---
+++ src/classes.py
@@ -0,0 +1,103 @@
+from abc import ABC, abstractmethod
+import json
+import requests
+
+
+class Headhunter_api(ABC):
+
+    @abstractmethod
+    def get_vacancies(self):
+        pass
+
+
+class Vacancy_json(ABC):
+
+    @abstractmethod
+    def vacancy_to_json(self, vacancy):
+        pass
+
+
+class Api_call(Headhunter_api):
+
+    def get_vacancies(self, key, request):
+        api_call = requests.get(key, {"text": request,
+                                "area": "113", "per_page": 2, "page": 1}).json()
+        return api_call
+
+    def __call__(self, key, request):
+        return self.get_vacancies(key, request)
+
+
+class Vacancy():
+
+    def __init__(self, response):
+        self.name = response["name"]
+        self.link = response["url"]
+
+        if not response["salary"]:
+            self.from_sal = 0
+            self.to_sal = 0
+            self.currency = "Валюта не указана"
+        else:
+            self.from_sal = response["salary"]["from"]
+            self.to_sal = response["salary"]["to"]
+            if response["salary"]["currency"] == "RUR":
+                self.currency = "RUR"
+            else:
+                data = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
+                data = data['Valute'][response["salary"]["currency"]]
+                if self.from_sal:
+                    self.from_sal *= data["Value"]
+                if self.to_sal:
+                    self.to_sal *= data["Value"]
+
+
+        self.requirement = response["snippet"]["requirement"]
+        self.experience_requirements = response["experience"]["name"]
+
+
+    def __eq__(self, other):
+        if self.from_sal and other.from_sal:
+            return self.from_sal == other.from_sal
+        if self.to_sal and other.to_sal:
+            return self.to_sal == other.to_sal
+        if self.from_sal and not other.from_sal:
+            return self.from_sal == other.to_sal
+        if not self.from_sal and other.from_sal:
+            return self.from_sal == other.to_sal
+
+    def __lt__(self, other):
+        if self.from_sal and other.from_sal:
+            return self.from_sal < other.from_sal
+        if self.to_sal and other.to_sal:
+            return self.to_sal < other.to_sal
+        if self.from_sal and not other.from_sal:
+            return self.from_sal < other.to_sal
+        if not self.from_sal and other.from_sal:
+            return self.from_sal < other.to_sal
+
+    def __gt__(self, other):
+        if self.from_sal and other.from_sal:
+            return self.from_sal > other.from_sal
+        if self.to_sal and other.to_sal:
+            return self.to_sal > other.to_sal
+        if self.from_sal and not other.from_sal:
+            return self.from_sal > other.to_sal
+        if not self.from_sal and other.from_sal:
+            return self.from_sal > other.to_sal
+
+    def __str__(self):
+        return f'{self.name}, {self.from_sal}-{self.to_sal},{self.experience_requirements}, {self.requirement}, {self.link}'
+
+
+class JSON_saver(Vacancy_json):
+
+    def vacancy_to_json(self, vacancies, path):
+        data = {"items": vacancies}
+        with open(path, 'w', encoding='utf-8') as json_file:
+            vac1 = json.dumps(data, ensure_ascii=False)
+            json_file.write(vac1)
+
+
+    def __call__(self, vacancies, path):
+        return self.vacancy_to_json(vacancies, path)

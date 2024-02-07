5eb17c108dd33e2119f11c11097f3bddd04d300a
Ivan Skliaruk <cunanori@gmail.com>
2024-02-07 18:10:15 +0100

    final cut

--- main.py
+++ main.py
@@ -1,37 +1,36 @@
-import requests
-import json
 from src.classes import Api_call, Vacancy, JSON_saver
 from pprint import pprint
 
 key = 'https://api.hh.ru/vacancies'
 
+to_json = JSON_saver()
+api_call_result = []
+
+def user_search(request):
+    """Задание ключевого слова для обращения к API, запись результатов в api_call_results"""
+    call = Api_call()
+    vacancies = []
+
+    for vac in call(key, request)["items"]:
+        vacancies.append(Vacancy(vac).__dict__)
+
+    api_call_result.extend(vacancies)
+
+
+def key_word_search(request):
+    """Задаем дополнительное ключевое слово для поиска среди полученных вакансий"""
+    vacansies_with_key = []
+    for vac in api_call_result:
+        for value in vac.values():
+            if isinstance(value, str) and request in value:
+                vacansies_with_key.append(vac)
+
+    return vacansies_with_key
+
+
+
 def user_interaction():
     """Функция взаимодействия с пользователем"""
-
-    to_json = JSON_saver()
-    api_call_result = []
-
-
-    def user_search(request):
-        """Задание ключевого слова для обращения к API, запись результатов в api_call_results"""
-        call = Api_call()
-        vacancies = []
-
-        for vac in call(key, request)["items"]:
-            vacancies.append(Vacancy(vac).__dict__)
-
-        api_call_result.extend(vacancies)
-
-
-    def key_word_search(request):
-        """Задаем дополнительное ключевое слово для поиска среди полученных вакансий"""
-        vacansies_with_key = []
-        for vac in api_call_result:
-            for value in vac.values():
-                if isinstance(value, str) and request in value:
-                    vacansies_with_key.append(vac)
-
-        return vacansies_with_key
 
     cnt = 0
     while not api_call_result:

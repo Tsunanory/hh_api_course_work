3fa83339e5ae5fd534f9b70e70f3aed24da01985
Ivan Skliaruk <cunanori@gmail.com>
2024-02-13 04:17:02 +0100

    key_word_search corrected

--- src/classes.py
+++ src/classes.py
@@ -1,6 +1,21 @@
 import json
 import requests
-from abs_classes import Headhunter_api, Vacancy_json
+from abc import ABC, abstractmethod
+# from abs_classes import Headhunter_api, Vacancy_json
+
+class Headhunter_api(ABC):
+    """Абстрактный метод, обязующий потомка определить метод получения вакансий"""
+    @abstractmethod
+    def get_vacancies(self):
+        pass
+
+
+class Vacancy_json(ABC):
+    """Абстрактный метод, обязующий потомка определить метод записи в JSON"""
+    @abstractmethod
+    def vacancy_to_json(self, vacancy):
+        pass
+
 
 
 class Api_call(Headhunter_api):
